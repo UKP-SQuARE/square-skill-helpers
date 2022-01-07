c0ca07542deb3d2a6a35e47e4229bfbfc8949f34
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-01-07 09:59:49 +0100

    adapt data api

--- square_skill_helpers/square_api.py
+++ square_skill_helpers/square_api.py
@@ -79,19 +79,22 @@ class ModelAPI(SquareAPI):
 
 
 class DataAPI(SquareAPI):
-
-    async def __call__(self, datastore: str, index_name: str, data_request):
+    async def __call__(
+        self, datastore_name: str, index_name: str, query: str, top_k: int = 10
+    ):
         """
-        Call the Data API with the given complete URL (http://<host>:<port>/datastore/<datastore_name>/indexs/<index_name>/search) and request.
-        The 'model_outputs' will be decoded if necessary and made into np arrays before returning.
-        :param url: The complete URL to send the request to
-        :param model_request: the request to use for the call
-        :return: The response from the Data API. If the request was not succesfull, an exception is raised.
+        Call /datastores/{datstore_name}/search
         """
-        url = f"{self.config.data_api_url}/{datastore}/indexs/{index_name}/search"
-        response = requests.get(url, params=data_request)
+        url = f"{self.config.data_api_url}/datastores/{datastore_name}/search"
+        response = requests.get(
+            url, params=dict(index_name=index_name, query=query, top_k=top_k)
+        )
         if response.status_code == 200:
-            return response.json()["root"]["children"]
+            return response.json()
         else:
-            raise RuntimeError(f"Request to data API at URL {url} with request {data_request} "
-                            f"failed with code {response.status_code} and message {response.text}")
+            raise RuntimeError(
+                f"Request to data API at URL {url} with request "
+                f"datastore_name={datastore_name} index_name={index_name} "
+                f"query={query} top_k={top_k} . failed with code "
+                f"{response.status_code} and message {response.text}"
+            )
