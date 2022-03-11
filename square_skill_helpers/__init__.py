7354a658a0fc0cd54b8c8aace9765b04e3e2e9c2
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-03-11 17:18:18 +0100

    Update __init__.py

--- square_skill_helpers/__init__.py
+++ square_skill_helpers/__init__.py
@@ -1,7 +1,6 @@
 from square_auth.client_credentials import ClientCredentials
+from square_skill_helpers.square_api import SquareAPI
 from square_skill_helpers.data_api import DataAPI
 from square_skill_helpers.model_api import ModelAPI
-from square_skill_helpers.square_api import SquareAPI
-
 
 client_credentials = ClientCredentials()
