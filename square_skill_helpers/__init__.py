58e704d1901f4236948db939adab377085555e25
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-07-21 10:16:55 +0200

    management client

--- square_skill_helpers/__init__.py
+++ square_skill_helpers/__init__.py
@@ -1,5 +1,6 @@
 from square_auth.client_credentials import ClientCredentials
 from square_skill_helpers.data_api import DataAPI
+# from square_skill_helpers.model_api_deprecated import ModelAPI
 from square_skill_helpers.model_api import ModelAPI
 from square_skill_helpers.square_api import SquareAPI
 
