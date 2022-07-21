aa021ff31f2001a792edf0957511b447efbd190c
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-07-21 11:49:02 +0200

    add comparison

--- square_skill_helpers/square_api.py
+++ square_skill_helpers/square_api.py
@@ -3,4 +3,4 @@ import os
 class SquareAPI():
     def __init__(self) -> None:
         self.square_api_url = os.getenv("SQUARE_API_URL")
-        self.verify_ssl = os.getenv("VERIFY_SSL")
+        self.verify_ssl = os.getenv("VERIFY_SSL") == "1"
